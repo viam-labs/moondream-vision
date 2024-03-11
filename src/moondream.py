e3546f8a3925f184b7007ecad0045ccd5a6d6ba1
Matt Vella <mcvella@gmail.com>
2024-03-11 09:41:31 -0400

    add moondream vision service

---
+++ src/moondream.py
@@ -0,0 +1,120 @@
+from typing import ClassVar, Mapping, Sequence, Any, Dict, Optional, Tuple, Final, List, cast
+from typing_extensions import Self
+
+from typing import Any, Final, List, Mapping, Optional, Union
+
+from PIL import Image
+
+from viam.media.video import RawImage
+from viam.proto.common import PointCloudObject
+from viam.proto.service.vision import Classification, Detection
+from viam.resource.types import RESOURCE_NAMESPACE_RDK, RESOURCE_TYPE_SERVICE, Subtype
+from viam.utils import ValueTypes
+
+
+from viam.module.types import Reconfigurable
+from viam.proto.app.robot import ComponentConfig
+from viam.proto.common import ResourceName, Vector3
+from viam.resource.base import ResourceBase
+from viam.resource.types import Model, ModelFamily
+
+from viam.services.vision import Vision
+from viam.components.camera import Camera
+from viam.logging import getLogger
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from PIL import Image
+
+LOGGER = getLogger(__name__)
+
+class moondream(Vision, Reconfigurable):
+    
+    """
+    Vision represents a Vision service.
+    """
+    
+
+    MODEL: ClassVar[Model] = Model(ModelFamily("viam-labs", "vision"), "moondream")
+    
+    model: AutoModelForCausalLM
+    tokenizer = AutoTokenizer
+
+    # Constructor
+    @classmethod
+    def new(cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]) -> Self:
+        my_class = cls(config.name)
+        my_class.reconfigure(config, dependencies)
+        return my_class
+
+    # Validates JSON Configuration
+    @classmethod
+    def validate(cls, config: ComponentConfig):
+        return
+
+    # Handles attribute reconfiguration
+    def reconfigure(self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]):
+        self.DEPS = dependencies
+        model_id = "vikhyatk/moondream2"
+        revision = config.attributes.fields["revision"].string_value or "2024-03-06"
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_id, trust_remote_code=True, revision=revision
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
+        return
+    
+    # not implemented, use classification methods
+    async def get_detections_from_camera(
+        self, camera_name: str, *, extra: Optional[Mapping[str, Any]] = None, timeout: Optional[float] = None
+    ) -> List[Detection]:
+        return
+
+     # not implemented, use classification methods
+    async def get_detections(
+        self,
+        image: Union[Image.Image, RawImage],
+        *,
+        extra: Optional[Mapping[str, Any]] = None,
+        timeout: Optional[float] = None,
+    ) -> List[Detection]:
+        return 
+    
+    async def get_classifications_from_camera(
+        self,
+        camera_name: str,
+        count: int,
+        *,
+        extra: Optional[Mapping[str, Any]] = None,
+        timeout: Optional[float] = None,
+    ) -> List[Classification]:
+        actual_cam = self.DEPS[Camera.get_resource_name(camera_name)]
+        cam = cast(Camera, actual_cam)
+        cam_image = await cam.get_image(mime_type="image/jpeg")
+        return await self.get_classifications(cam_image, count, extra=extra)
+
+    
+    async def get_classifications(
+        self,
+        image: Union[Image.Image, RawImage],
+        count: int,
+        *,
+        extra: Optional[Mapping[str, Any]] = None,
+        timeout: Optional[float] = None,
+    ) -> List[Classification]:
+        classifications = []
+        question = "describe this image"
+        if extra != None and extra.get('question') != None:
+            question = extra['question']
+        enc_image = self.model.encode_image(image)
+        result = self.model.answer_question(enc_image, question, self.tokenizer)
+        classifications.append({"class_name": result, "confidence": 1})
+        return classifications
+
+    
+    async def get_object_point_clouds(
+        self, camera_name: str, *, extra: Optional[Mapping[str, Any]] = None, timeout: Optional[float] = None
+    ) -> List[PointCloudObject]:
+        return
+    
+    async def do_command(self, command: Mapping[str, ValueTypes], *, timeout: Optional[float] = None) -> Mapping[str, ValueTypes]:
+        return
+
