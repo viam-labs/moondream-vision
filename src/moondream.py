e8829b20fffcb684f349fc088e557c242fd681a4
Matt Vella <mcvella@gmail.com>
2024-05-24 16:44:56 -0400

    Changes for Viam Python SDK breaking changes

--- src/moondream.py
+++ src/moondream.py
@@ -5,7 +5,6 @@ from typing import Any, Final, List, Mapping, Optional, Union
 
 from PIL import Image
 
-from viam.media.video import RawImage
 from viam.proto.common import PointCloudObject
 from viam.proto.service.vision import Classification, Detection
 from viam.resource.types import RESOURCE_NAMESPACE_RDK, RESOURCE_TYPE_SERVICE, Subtype
@@ -14,12 +13,14 @@ from viam.utils import ValueTypes
 
 from viam.module.types import Reconfigurable
 from viam.proto.app.robot import ComponentConfig
-from viam.proto.common import ResourceName, Vector3
+from viam.proto.common import ResourceName
 from viam.resource.base import ResourceBase
 from viam.resource.types import Model, ModelFamily
 
-from viam.services.vision import Vision
-from viam.components.camera import Camera
+from viam.services.vision import Vision, CaptureAllResult
+from viam.proto.service.vision import GetPropertiesResponse
+from viam.components.camera import Camera, ViamImage
+from viam.media.utils.pil import viam_to_pil_image
 from viam.logging import getLogger
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
@@ -62,6 +63,15 @@ class moondream(Vision, Reconfigurable):
         self.tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
         return
     
+    async def get_cam_image(
+        self,
+        camera_name: str
+    ) -> Image:
+        actual_cam = self.DEPS[Camera.get_resource_name(camera_name)]
+        cam = cast(Camera, actual_cam)
+        cam_image = await cam.get_image(mime_type="image/jpeg")
+        return viam_to_pil_image(cam_image)
+    
     # not implemented, use classification methods
     async def get_detections_from_camera(
         self, camera_name: str, *, extra: Optional[Mapping[str, Any]] = None, timeout: Optional[float] = None
@@ -71,7 +81,7 @@ class moondream(Vision, Reconfigurable):
      # not implemented, use classification methods
     async def get_detections(
         self,
-        image: Union[Image.Image, RawImage],
+        image: ViamImage,
         *,
         extra: Optional[Mapping[str, Any]] = None,
         timeout: Optional[float] = None,
@@ -86,15 +96,12 @@ class moondream(Vision, Reconfigurable):
         extra: Optional[Mapping[str, Any]] = None,
         timeout: Optional[float] = None,
     ) -> List[Classification]:
-        actual_cam = self.DEPS[Camera.get_resource_name(camera_name)]
-        cam = cast(Camera, actual_cam)
-        cam_image = await cam.get_image(mime_type="image/jpeg")
-        return await self.get_classifications(cam_image, count, extra=extra)
+        return await self.get_classifications(await self.get_cam_image(camera_name), count, extra=extra)
 
     
     async def get_classifications(
         self,
-        image: Union[Image.Image, RawImage],
+        image: ViamImage,
         count: int,
         *,
         extra: Optional[Mapping[str, Any]] = None,
@@ -104,7 +111,7 @@ class moondream(Vision, Reconfigurable):
         question = "describe this image"
         if extra != None and extra.get('question') != None:
             question = extra['question']
-        enc_image = self.model.encode_image(image)
+        enc_image = self.model.encode_image(viam_to_pil_image(image))
         result = self.model.answer_question(enc_image, question, self.tokenizer)
         classifications.append({"class_name": result, "confidence": 1})
         return classifications
@@ -118,3 +125,30 @@ class moondream(Vision, Reconfigurable):
     async def do_command(self, command: Mapping[str, ValueTypes], *, timeout: Optional[float] = None) -> Mapping[str, ValueTypes]:
         return
 
+    async def capture_all_from_camera(
+        self,
+        camera_name: str,
+        return_image: bool = False,
+        return_classifications: bool = False,
+        return_detections: bool = False,
+        return_object_point_clouds: bool = False,
+        *,
+        extra: Optional[Mapping[str, Any]] = None,
+        timeout: Optional[float] = None,
+    ) -> CaptureAllResult:
+        result = CaptureAllResult()
+        result.image = await self.get_cam_image(camera_name)
+        result.classifications = await self.get_classifications(result.image, 1)
+        return result
+
+    async def get_properties(
+        self,
+        *,
+        extra: Optional[Mapping[str, Any]] = None,
+        timeout: Optional[float] = None,
+    ) -> GetPropertiesResponse:
+        return GetPropertiesResponse(
+            classifications_supported=True,
+            detections_supported=False,
+            object_point_clouds_supported=False
+            )
